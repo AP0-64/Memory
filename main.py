dab409ee07637d9b09a52ed32bc0fb15bf95dfea
Paolo BERTINI <bertin_p@etna-alternance.net>
2025-06-05 16:31:11 +0200

    réarangement

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from random import randint
 from time import sleep
 
+
 def genere_liste_aleatoire(niveau):
-    """ Génère une liste de longueur n avec des élements aléatoires """
+    """ Génère une liste de longueur n avec des éléments aléatoires """
     assert niveau > 0
     global lst_aleatoire
     lst_aleatoire = []
-    for _ in range(niveau):
-        lst_aleatoire.append(randint(10, 99))
+    lst_aleatoire.extend(randint(10, 99) for _ in range(niveau))
     return lst_aleatoire
+
 
 def saisie_liste(niveau):
     """ Génère une liste de longueur n en demandant les entiers voulus """
@@ -17,7 +18,8 @@ def saisie_liste(niveau):
     for i in range(niveau):
         while True:
             try:
-                entier_saisie = int(input(f"Veuillez taper l'entier numéro {i+1} : "))
+                entier_saisie = int(input(
+                    f"Veuillez taper l'entier numéro {i+1} : "))
                 lst_saisie.append(entier_saisie)
                 break
             except ValueError:
@@ -33,36 +35,62 @@ def mystere(lst, n):
             compteur = compteur + 1
     return compteur
 
+
 def compare_liste(lst1, lst2):
     """ Compare deux listes et renvoie le nombre d'erreurs """
     assert len(lst1) == len(lst2)
-    nombre_de_difference = 0
-    for i in range(len(lst1)):
-        if lst1[i] != lst2[i]:
-            nombre_de_difference += 1
-    return nombre_de_difference
+    return sum(a != b for a, b in zip(lst1, lst2))
+
 
 def affichage():
     print("\n" * 40)
     print("Bienvenu au jeu de mémoire.")
-    print()
-    print("Objectif : Le but du jeu est de tester ta mémoire et de voir jusqu'à quel niveau tu peux aller en reproduisant des séquences de nombres qui sera de plus en plus longue à travers les niveaux.")
-    print()
-    print("À chaque niveau, tu verras une séquence de nombres s'afficher à l'écran. Regarde bien cette séquence, car tu aura quelques secondes pour la mémoriser. Ensuite, tu devras taper une liste de nombres.")
-    print()
-    print("Si tu parviens à reproduire la séquence exactement comme elle était affichée, tu passes au niveau suivant, et tu gagnes ! Du moins, pour l'instant.")
+    _extracted_from_affichage_4(
+        "Objectif : ",
+        "Le but du jeu est de tester ta mémoire et de voir jusqu'à",
+        "quel niveau tu peux aller en reproduisant des séquences",
+    )
+    print("de nombres qui seront de plus en plus longues à travers")
+    print("les niveaux.")
+    _extracted_from_affichage_4(
+        "À chaque niveau, tu verras une séquence de nombres s'afficher",
+        " à l'écran. Regarde bien cette séquence, car tu aura quelques",
+        "secondes pour la mémoriser. Ensuite, tu devras taper une liste",
+    )
+    print("de nombres. Tape un nombre à la fois, en appuyant sur entrer.")
+    _extracted_from_affichage_4(
+        "Si tu parviens à reproduire la séquence exactement comme elle",
+        "était affichée, tu passes au niveau suivant, et tu gagnes !",
+        "Du moins, pour l'instant.",
+    )
     print()
     print("Si tu te trompes en tapant la liste de nombres, tu perdra.")
-    print()
-    print("L'objectif ultime est de voir jusqu'à quel niveau tu peux arriver en te rappelant et en tapant correctement ces séquences de nombres.")
+    _extracted_from_affichage_4(
+        "L'objectif ultime est de voir jusqu'à quel niveau tu peux",
+        "arriver en te rappelant et en tapant correctement ces séquences",
+        "de nombres.",
+    )
     print()
 
+
+# TODO Rename this here and in `affichage`
+def _extracted_from_affichage_4(arg0, arg1, arg2):
+    print()
+    print(arg0)
+    print(arg1)
+    print(arg2)
+
+
 niveau = 0
+
+# Programme principal
 while True:
     if niveau == 0:
         affichage()
-    input("Appuie sur entrer pour continuer.")
     niveau += 1
+    if niveau != 0:
+        print("Niveau", niveau)
+    input("Appuie sur entrer pour continuer.")
     cible = genere_liste_aleatoire(niveau)
     print(cible)
     sleep(niveau + 1)
